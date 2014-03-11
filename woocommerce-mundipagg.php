119c1ede22c3a4903a8934cfe3503a47735db3fd
claudiosmweb <contato@claudiosmweb.com>
2014-03-11 15:39:00 -0300

    added initial files

---
+++ woocommerce-mundipagg.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Plugin Name: WooCommerce MundiPagg
+ * Plugin URI: http://claudiosmweb.com
+ * Description: MundiPagg gateway for WooCommerce
+ * Author: Claudio Sanches
+ * Author URI: http://claudiosmweb.com/
+ * Version: 1.0.0
+ * License: GPLv2 or later
+ * Text Domain: woocommerce-mundipagg
+ * Domain Path: /languages/
+ */
