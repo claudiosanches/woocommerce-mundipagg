6e49fdd3aeeffeccccc132b74c7d0c97e9472f55
Claudio Sanches <contato@claudiosmweb.com>
2016-02-14 23:30:31 -0200

    Updated grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,10 +1,12 @@
-/*jshint node:true */
+/* jshint node:true */
+var expandHomeDir = require( 'expand-home-dir' );
+
 module.exports = function( grunt ) {
 'use strict';
 
 	grunt.initConfig({
 
-		// Setting folder templates.
+		// Setting folder templates
 		dirs: {
 			css:    'assets/css',
 			fonts:  'assets/fonts',
@@ -12,26 +14,40 @@ module.exports = function( grunt ) {
 			js:     'assets/js'
 		},
 
-		// Gets the package vars.
+		// gets the package vars
 		pkg: grunt.file.readJSON( 'package.json' ),
+		svn_settings: {
+			path: expandHomeDir( '~/Projects/wordpress-plugins-svn/' ) + '<%= pkg.name %>',
+			tag: '<%= svn_settings.path %>/tags/<%= pkg.version %>',
+			trunk: '<%= svn_settings.path %>/trunk',
+			exclude: [
+				'.git/',
+				'.tx/',
+				'.editorconfig',
+				'.gitignore',
+				'.jshintrc',
+				'node_modules/',
+				'Gruntfile.js',
+				'README.md',
+				'package.json',
+				'*.zip'
+			]
+		},
 
-		// Javascript linting with jshint.
+		// Javascript linting with jshint
 		jshint: {
 			options: {
 				jshintrc: '.jshintrc'
 			},
 			all: [
 				'Gruntfile.js',
-				'<%= dirs.js %>/*.js',
-				'!<%= dirs.js %>/*.min.js'
+				'<%= dirs.js %>/*/*.js',
+				'!<%= dirs.js %>/*/*.min.js'
 			]
 		},
 
 		// Minify .js files.
 		uglify: {
-			options: {
-				preserveComments: 'some'
-			},
 			dist: {
 				files: [{
 					expand: true,
@@ -46,8 +62,29 @@ module.exports = function( grunt ) {
 			}
 		},
 
+		// Minify all .css files.
+		cssmin: {
+			minify: {
+				expand: true,
+				cwd: '<%= dirs.css %>/',
+				src: [
+					'*.css',
+					'!*.min.css'
+				],
+				dest: '<%= dirs.css %>/',
+				ext: '.min.css'
+			}
+		},
+
 		// Watch changes for assets
 		watch: {
+			css: {
+				files: [
+					'<%= dirs.css %>/*.css',
+					'!<%= dirs.css %>/*.min.css'
+				],
+				tasks: ['cssmin']
+			},
 			js: {
 				files: [
 					'<%= dirs.js %>/*js',
@@ -55,17 +92,149 @@ module.exports = function( grunt ) {
 				],
 				tasks: ['jshint', 'uglify']
 			}
+		},
+
+		// Image optimization
+		imagemin: {
+			dist: {
+				options: {
+					optimizationLevel: 7,
+					progressive: true
+				},
+				files: [{
+					expand: true,
+					cwd: './',
+					src: 'screenshot-*.png',
+					dest: './'
+				}]
+			}
+		},
+
+		// Make .pot files
+		makepot: {
+			dist: {
+				options: {
+					type: 'wp-plugin'
+				}
+			}
+		},
+
+		// Check text domain
+		checktextdomain: {
+			options:{
+				text_domain: '<%= pkg.name %>',
+				keywords: [
+					'__:1,2d',
+					'_e:1,2d',
+					'_x:1,2c,3d',
+					'esc_html__:1,2d',
+					'esc_html_e:1,2d',
+					'esc_html_x:1,2c,3d',
+					'esc_attr__:1,2d',
+					'esc_attr_e:1,2d',
+					'esc_attr_x:1,2c,3d',
+					'_ex:1,2c,3d',
+					'_n:1,2,4d',
+					'_nx:1,2,4c,5d',
+					'_n_noop:1,2,3d',
+					'_nx_noop:1,2,3c,4d'
+				]
+			},
+			files: {
+				src:  [
+					'**/*.php', // Include all files
+					'!node_modules/**' // Exclude node_modules/
+				],
+				expand: true
+			}
+		},
+
+		// Rsync commands used to take the files to svn repository
+		rsync: {
+			options: {
+				args: ['--verbose'],
+				exclude: '<%= svn_settings.exclude %>',
+				syncDest: true,
+				recursive: true
+			},
+			tag: {
+				options: {
+					src: './',
+					dest: '<%= svn_settings.tag %>'
+				}
+			},
+			trunk: {
+				options: {
+				src: './',
+				dest: '<%= svn_settings.trunk %>'
+				}
+			}
+		},
+
+		// Shell command to commit the new version of the plugin
+		shell: {
+			// Remove delete files.
+			svn_remove: {
+				command: 'svn st | grep \'^!\' | awk \'{print $2}\' | xargs svn --force delete',
+				options: {
+					stdout: true,
+					stderr: true,
+					execOptions: {
+						cwd: '<%= svn_settings.path %>'
+					}
+				}
+			},
+			// Add new files.
+			svn_add: {
+				command: 'svn add --force * --auto-props --parents --depth infinity -q',
+				options: {
+					stdout: true,
+					stderr: true,
+					execOptions: {
+						cwd: '<%= svn_settings.path %>'
+					}
+				}
+			},
+			// Commit the changes.
+			svn_commit: {
+				command: 'svn commit -m "updated the plugin version to <%= pkg.version %>"',
+				options: {
+					stdout: true,
+					stderr: true,
+					execOptions: {
+						cwd: '<%= svn_settings.path %>'
+					}
+				}
+			}
 		}
+
 	});
 
 	// Load tasks
+	grunt.loadNpmTasks( 'grunt-contrib-watch' );
 	grunt.loadNpmTasks( 'grunt-contrib-jshint' );
 	grunt.loadNpmTasks( 'grunt-contrib-uglify' );
-	grunt.loadNpmTasks( 'grunt-contrib-watch' );
+	grunt.loadNpmTasks( 'grunt-contrib-cssmin' );
+	grunt.loadNpmTasks( 'grunt-contrib-imagemin' );
+	grunt.loadNpmTasks( 'grunt-checktextdomain' );
+	grunt.loadNpmTasks( 'grunt-wp-i18n' );
+
+	grunt.loadNpmTasks( 'grunt-rsync' );
+	grunt.loadNpmTasks( 'grunt-shell' );
 
 	// Register tasks
 	grunt.registerTask( 'default', [
 		'jshint',
 		'uglify'
 	]);
+
+	// Deploy task
+	grunt.registerTask( 'deploy', [
+		'default',
+		'rsync:tag',
+		'rsync:trunk',
+		'shell:svn_remove',
+		'shell:svn_add',
+		'shell:svn_commit'
+	] );
 };
